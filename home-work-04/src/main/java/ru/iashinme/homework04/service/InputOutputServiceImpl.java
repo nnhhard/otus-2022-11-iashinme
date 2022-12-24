3d669d42f255ab67eab9e496242c337087c90b27
IashinME <iashinme@alfastrah.ru>
2022-12-24 10:53:00 +0700

    Opened InputOutputServiceImpl constructor with InputStream and PrintStream

--- home-work-04/src/main/java/ru/iashinme/homework04/service/InputOutputServiceImpl.java
+++ home-work-04/src/main/java/ru/iashinme/homework04/service/InputOutputServiceImpl.java
@@ -1,26 +1,36 @@
 package ru.iashinme.homework04.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.InputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 @Service
 public class InputOutputServiceImpl implements InputOutputService {
 
+    private final Scanner scan;
+    private final PrintStream out;
+
+    public InputOutputServiceImpl(@Value("#{ T(java.lang.System).in}") InputStream in,
+                                  @Value("#{ T(java.lang.System).out}") PrintStream out) {
+        this.out = out;
+        this.scan = new Scanner(in);
+    }
+
     @Override
     public void printMessage(String message) {
-            System.out.println(message);
+            out.println(message);
     }
 
     @Override
-    public String readLine(){
-        Scanner in = new Scanner(System.in);
-        return in.nextLine();
+    public String readLine() {
+        return scan.nextLine();
     }
 
     @Override
-    public int readInt(){
-        Scanner in = new Scanner(System.in);
-        return in.nextInt();
+    public int readInt() {
+        return scan.nextInt();
     }
 }
